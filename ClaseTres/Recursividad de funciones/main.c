f4ed5ac0a0368b7bf99a98ee5ecc45477759a7eb
alumno <alumno@utnfra.com.ar>
2019-09-03 12:05:45 -0300

    factorial con recursividad

--- ClaseTres/Recursividad de funciones/main.c
+++ ClaseTres/Recursividad de funciones/main.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int factorial(int);
+
 int main()
 {
 
     int numeroUno;
-    int resultado = 1;
-    int i;
+    int resultado ;
 
     printf("Ingrese un numero: ");
     scanf("%d", &numeroUno);
 
-    for(i = numeroUno; i > 0 ; i--)
-    {
+    resultado = factorial(numeroUno);
 
-        resultado = resultado * i;
-
-    }
     printf("El factorial de %d es : %d", numeroUno, resultado);
 
     return 0;
 }
+int factorial(int numero)
+{
+    int resultado;
 
+    if(numero == 0)
+    {
+        resultado = 1;
+    }
+    else
+    {
+        resultado = numero * factorial(numero - 1);
+    }
+    return resultado;
+}
 
 
 
